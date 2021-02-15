72461c05ab398c7300cef349fac99c1cfd5ae4b1
KitomaGithub <kitomahiragashi@gmail.com>
2021-02-15 14:55:11 +0800

    Update and rename index.html to index.php

---
+++ web/index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+    <head>
+      <title> Website </title>
+    </head>
+    <body>
+      <form action="hello.php" method="get">
+          <h1> Simple PHP program </h1>
+          Full Name:<input type="text" name="name"><br>
+          Email Address:<input type name="email">
+          <input type="submit">
+      </form>
+    </body>
+</html>
