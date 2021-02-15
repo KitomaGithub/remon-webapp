c26cb568f06e7fef3210bcdf5ee4b01f3230316d
KitomaGithub <kitomahiragashi@gmail.com>
2021-02-15 14:27:28 +0800

    Update index.php

--- web/index.php
+++ web/index.php
@@ -1,11 +1,11 @@
 <!DOCTYPE html>
 <html>
     <head>
-      <title> Hello Maning </title>
+      <title> Website </title>
     </head>
     <body>
-      <h1 style="font-size:50px;">
-        HELLO MANING!
+      <h1 style="font-size:50px; color:red;">
+        HELLO DARL!
       </h1>
     </body>
 </html>
