88677127c53373c4c5622c18a9668579e1c34667
KitomaGithub <kitomahiragashi@gmail.com>
2021-02-15 15:26:22 +0800

    Update index.php

--- web/index.php
+++ web/index.php
@@ -1,19 +1,9 @@
-<!DOCTYPE html>
-<html>
-    <head>
-      <title> Website </title>
-    </head>
-    <body>
-        <pre>
-            <?php
-                $pdo = new PDO('sqlite:master.db');
-                
-                $state = $pdo->query("SELECT * FROM ability_list");
-                    
-                $rows = $state->fetchAll(PDO::FETCH_ASSOC);
+<?php
+$pdo = new PDO('sqlite:master.db');
 
-                var_dump($rows);
-            ?>
-        </pre>
-    </body>
-</html>
+$statement = $pdo->query("SELECT * FROM ability_list");
+
+$rows = $statement->fetchAll(PDO::FETCH_ASSOC);
+
+var_dump($rows);
+?>
