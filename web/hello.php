a78b946534c7f8c8c4ef7692ba452dc6f106d90f
KitomaGithub <kitomahiragashi@gmail.com>
2021-02-15 14:59:50 +0800

    Create hello.php

---
+++ web/hello.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+  <head> 
+    <title> Hello! </title>
+  </head>
+  <body>
+    Welcome <?php echo $_GET["name"]; ?> <br>
+    Your Email address is: <?php echo $_GET["email"]; ?>
+  </body>
+</html>
